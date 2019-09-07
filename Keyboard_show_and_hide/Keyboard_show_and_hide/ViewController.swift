89139f4517b6fab1321a935cb3bd617de1d91291
Bumjin Park <bjpark4002@gmail.com>
2019-09-07 01:52:53 -0700

    Keyboard and cursor to first text field.
    Keyboard hide + first responder

---
+++ Keyboard_show_and_hide/Keyboard_show_and_hide/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  Keyboard_show_and_hide
+//
+//  Created by Bumjin Park on 9/7/19.
+//  Copyright © 2019 Bumjin Park. All rights reserved.
+//
+
+import UIKit
+
+
+class DetailViewController: UIViewController{
+    @IBOutlet weak var myTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        myTextField.becomeFirstResponder()
+    }
+    
+//    override func viewWillAppear(_ animated: Bool) {
+//        super.viewWillAppear(animated)
+//        myTextField.becomeFirstResponder()
+//
+//    }
+//    override func viewDidAppear(_ animated: Bool) {
+//        super.viewDidAppear(animated)
+//        super.viewWillAppear(animated)
+//        myTextField.becomeFirstResponder()
+//
+//    }
+    
+    @IBAction func hideKeyBoard(_ sender: Any) {
+        myTextField.resignFirstResponder()
+        //myTextField.endEditing(true)
+    }
+}
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
